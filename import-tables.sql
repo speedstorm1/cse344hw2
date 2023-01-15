f2a576f00554303d576e2dbdef780e000655fa26
Ayush <speedstorm123@gmail.com>
2023-01-15 13:12:41 -0800

    fixed syntax on import-tables

--- import-tables.sql
+++ import-tables.sql
@@ -1,6 +1,6 @@
 PRAGMA foreign_keys=ON;
 .mode csv
-import flight-dataset/carriers.csv Carriers;
-import flight-dataset/months.csv Months;
-import flight-dataset/weekdays.csv Weekdays;
-import flight-dataset/flights-small.csv Flights;
+.import flight-dataset/carriers.csv Carriers;
+.import flight-dataset/months.csv Months;
+.import flight-dataset/weekdays.csv Weekdays;
+.import flight-dataset/flights-small.csv Flights;
